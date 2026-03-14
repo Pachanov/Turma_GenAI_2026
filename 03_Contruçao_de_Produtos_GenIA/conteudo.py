7701d269050d5f44e064c1fc9db37fc9d66ba16b
VICTOR HUGO PACHANO MAURERA <vhpmaurera@inf.ufpr.br>
2026-03-14 09:01:17 -0300

    Quarta Aula

---
+++ 03_Contruçao_de_Produtos_GenIA/conteudo.py
@@ -0,0 +1,37 @@
+##########Trabalho em equipe##########
+
+#projetos de GenIA raramente sao feitos por uma única pessoa.
+#Conhecimento compartilhado:Evita o fator onibus(se alguem sai, o projeto continua)
+#Revisao de pares: fundamental para garanttir a etica e performance do modelo.
+#escalabilidade: Facilita a adicao de novos membros e a distribuiçao de tarefas.
+
+##########Organizacao de codigo##########
+
+#-Organizacao de codigo é crucial para a manutenibilidade e colaboracao em projetos de GenIA.
+
+#Boas praticas:
+#Estrutura de pastas: Organize o codigo em pastas logicas (ex: data, models, utils).
+#Readme.md: Fornece uma visao geral do projeto, instrucoes de instalacao e uso.
+#Nomenclatura sistematica e desciptiva: nomear arquivos e script de forma ordenada e clara.
+
+##########Documentacao e manutençao de codigo##########
+
+#Documentaçao do codigo: Inclua comentarios claros e detalhados para explicar a logica e o funcionamento do codigo.
+#controle de versao: Use sistemas de controle de versao (ex: Git) para acompanhar as mudancas no codigo e facilitar a colaboracao.
+#Testes automatizados: Implemente testes automatizados para garantir a qualidade e confiabilidade
+#ambiente virtual isolado: Use ambientes virtuais (ex: virtualenv, conda) para gerenciar dependencias e evitar conflitos entre projetos.
+#scrips principais e scripts auxiliares: Separe scripts principais (ex: treinamento, avaliacao) de scripts auxiliares (ex: preprocessamento, visualizacao) para melhorar a organizacao e facilitar a manutencao.
+
+
+##########Reprudutibilidade##########
+
+#roda na minha maquina nao e reprodutibilidade
+#ambientes computaçionais variam entre usuarios e sistemas
+#versoes diferentes de pacotes e dependencias podem levar a resultados diferentes
+#reprodutibilidade é fundamental para:
+#1)transparencia cientifica: Permite que outros pesquisadores verifiquem e validem os resultados.
+#2)verificaçao de resultados: Facilita a detecçao de erros e a melhoria do modelo.
+#3)colaboraçao: Facilita a colaboraçao entre pesquisadores e equipes, permitindo
+#4)ciclo de vida do projeto: Facilita a manutencao e atualizaçao do projeto ao longo do tempo.
+
+
